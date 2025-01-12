6295bf378154c2abbe35af5b7c95496701442827
uttom_kumar <uttom00781@gmail.com>
2025-01-12 23:56:54 +0600

    add server

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(helmet())
 app.use(cors(
     {
         // front-end url
-        origin: "https://z-media-tan.vercel.app",
+        origin: "https://z-media-rge4.vercel.app",
         credentials: true
     }
 ))
