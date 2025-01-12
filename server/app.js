f3b1d731c9b14c68a3b35aed811af9e3eaab1ad7
uttom_kumar <uttom00781@gmail.com>
2025-01-12 23:52:18 +0600

    add

--- server/app.js
+++ server/app.js
@@ -20,7 +20,13 @@ import cookieParser from 'cookie-parser'
 app.use(bodyParser.json())
 app.use(mongoSanitize())
 app.use(helmet())
-app.use(cors())
+app.use(cors(
+    {
+        // front-end url
+        origin: "https://z-media-tan.vercel.app",
+        credentials: true
+    }
+))
 app.use(hpp())
 app.use(cookieParser())
 // TODO : RATE LIMITER CONFIGURATION IMPLEMENT
