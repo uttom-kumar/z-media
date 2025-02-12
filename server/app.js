4ef533c92706250da6e444cce3e53ea2b599e107
uttom_kumar <uttom00781@gmail.com>
2025-02-13 04:31:04 +0600

    some change

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(mongoSanitize())
 app.use(helmet())
 app.use(cors(
     {
-        origin: 'https://zmedia-fawn.vercel.app',
+        // origin: 'https://zmedia-fawn.vercel.app',
         // origin: 'http://localhost:5173',
         methods: ["GET","HEAD","PUT","POST","DELETE","OPTIONS"],
         credentials: true
