0a5c6f8f59c3c9209068d5fced54edb422c1b2b4
uttom_kumar <uttom00781@gmail.com>
2025-02-15 17:22:20 +0600

    s

--- server/app.js
+++ server/app.js
@@ -25,8 +25,9 @@ app.use(helmet())
 
 app.use(cors({
   origin: [
-    // 'http://localhost:5173',
-    "https://astonishing-sunshine-1d9865.netlify.app"
+    'http://localhost:5173',
+    "https://uviom.searchfriend.pro",
+    // "https://astonishing-sunshine-1d9865.netlify.app"
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
