07757ed6de240f31255995ab79e5c544b85e5fd3
uttom_kumar <uttom00781@gmail.com>
2025-02-15 14:42:54 +0600

    s

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,34 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
+
+
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
   ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss],
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Example of manual chunking to split large libraries
-          vendor: ['react', 'react-dom', 'axios'], // Add other large dependencies here
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Adjust chunk size warning limit to 1000 KB (default is 500 KB)
-  },
   server: {
     proxy: {
       '/api/': {
-        target: 'https://z-media-api.onrender.com',
+        target:'https://z-media-api.onrender.com',
         changeOrigin: true,
         secure: false,
-      },
-    },
-  },
+      }
+    }
+  }
 })
